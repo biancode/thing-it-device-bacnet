6cb143dd9a1fd28e680a314c2410499a9d5b5e84
Geass <geass1000@gmail.com>
2018-04-25 19:59:42 +0300

    add: "isEqualObjectId" method

--- src/core/bacnet/utils/bacnet.util.ts
+++ src/core/bacnet/utils/bacnet.util.ts
@@ -4,6 +4,8 @@ import { blvc, npdu, apdu } from '../layers';
 
 import { ILayer, ILayerBLVC } from '../interfaces';
 
+import * as BACnetTypes from '../types';
+
 export class BACnetUtil {
 
     static bufferToLayer (buf: Buffer): ILayer {
@@ -13,5 +15,11 @@ export class BACnetUtil {
             npdu: _.get(blvcMessage, 'npdu'),
             apdu: _.get(blvcMessage, 'npdu.apdu'),
         };
+    }
+
+    static isEqualObjectId (objId1: BACnetTypes.BACnetObjectId,
+            objId2: BACnetTypes.BACnetObjectId): boolean {
+        return objId1.value.type === objId2.value.type
+            && objId1.value.instance === objId2.value.instance;
     }
 }
