5b1af523a40e4a0195e6d6b5d2c2648083e5dcd5
Geass <geass1000@gmail.com>
2018-04-25 18:22:51 +0300

    add: "BACnetUtil" utility

---
+++ src/core/bacnet/utils/bacnet.util.ts
@@ -0,0 +1,17 @@
+import * as _ from 'lodash';
+
+import { blvc, npdu, apdu } from '../layers';
+
+import { ILayer, ILayerBLVC } from '../interfaces';
+
+export class BACnetUtil {
+
+    static bufferToLayer (buf: Buffer): ILayer {
+        let blvcMessage: ILayerBLVC = blvc.getFromBuffer(buf);
+        return {
+            blvc: blvcMessage,
+            npdu: _.get(blvcMessage, 'npdu'),
+            apdu: _.get(blvcMessage, 'npdu.apdu'),
+        };
+    }
+}
