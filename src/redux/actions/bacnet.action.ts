bb611a337343bb3c684c957b806d4770c76c37db
Geass <geass1000@gmail.com>
2018-05-02 13:21:12 +0300

    add: "set" operation (acton/event/reducer) for Logger utility

--- src/redux/actions/bacnet.action.ts
+++ src/redux/actions/bacnet.action.ts
@@ -3,9 +3,24 @@ import { BACnetEvent } from '../events/bacnet.event';
 import { IAction } from '../core/redux.interface';
 
 import { BACnetFlowManager, BACnetServiceManager } from '../../core/managers';
+import { Logger } from '../../core/utils';
 import { ServerSocket } from '../../core/sockets';
 
 export class BACnetAction {
+
+    /**
+     *  Sets the instance of the TID logger utility to `redux` store.
+     *
+     * @static
+     * @param  {Logger} logger - instance of the TID logger utility
+     * @return {IAction}
+     */
+    static setTIDLogger (logger: Logger): IAction {
+        return store.dispatch({
+            type: BACnetEvent.setTIDLogger,
+            payload: { logger: logger },
+        });
+    }
 
     /**
      *  Sets the instance of the BACnet Flow Manager to `redux` store.
@@ -17,7 +32,7 @@ export class BACnetAction {
     static setBACnetFlowManager (manager: BACnetFlowManager): IAction {
         return store.dispatch({
             type: BACnetEvent.setBACnetFlowManager,
-            payload: { manager : manager },
+            payload: { manager: manager },
         });
     }
 
@@ -31,7 +46,7 @@ export class BACnetAction {
     static setBACnetServiceManager (manager: BACnetServiceManager): IAction {
         return store.dispatch({
             type: BACnetEvent.setBACnetServiceManager,
-            payload: { manager : manager },
+            payload: { manager: manager },
         });
     }
 
@@ -45,7 +60,7 @@ export class BACnetAction {
     static setBACnetServer (server: ServerSocket): IAction {
         return store.dispatch({
             type: BACnetEvent.setBACnetServiceManager,
-            payload: { server : server },
+            payload: { server: server },
         });
     }
 }
