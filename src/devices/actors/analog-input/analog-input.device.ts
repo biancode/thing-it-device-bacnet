d1570066e54bb2d0d3c934244a26b46af93a9755
Geass <geass1000@gmail.com>
2018-05-04 17:47:48 +0300

    add: logic to get `flow` and `service` managers

--- src/devices/actors/analog-input/analog-input.device.ts
+++ src/devices/actors/analog-input/analog-input.device.ts
@@ -8,14 +8,23 @@ import {
     IAnalogInputActorConfig,
 } from '../../../core/interfaces';
 
+import {
+    BACnetFlowManager,
+    BACnetServiceManager,
+} from '../../../core/managers';
+
 export class AnalogInputActorDevice extends ActorDevice {
     public readonly className: string = 'AnalogInputActorDevice';
     public state: IAnalogInputActorState;
     public config: IAnalogInputActorConfig;
 
+    public flowManager: BACnetFlowManager;
+    public serviceManager: BACnetServiceManager;
     public async initDevice (): Promise<any> {
         await super.initDevice();
 
+        this.flowManager = store.getState([ 'bacnet', 'flowManager' ]);
+        this.serviceManager = store.getState([ 'bacnet', 'serviceManager' ]);
         this.state.initialized = true;
         this.publishStateChange();
     }
