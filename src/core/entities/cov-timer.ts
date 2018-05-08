888e686fc5bf28f2dcbe7766576f793a637f9a68
Geass <geass1000@gmail.com>
2018-05-08 13:09:32 +0300

    add: `valueOf` method and comments

--- src/core/entities/cov-timer.ts
+++ src/core/entities/cov-timer.ts
@@ -9,9 +9,24 @@ export class COVTimer {
     public prev: Moment;
     public next: Moment;
 
-    public init (config: Interfaces.COVTimer.Config) {
+    /**
+     * Inits the COVTimer entity.
+     *
+     * @param  {Interfaces.COVTimer.Config} config - entity configuration
+     * @return {void}
+     */
+    public init (config: Interfaces.COVTimer.Config): void {
         this.config = config;
         this.prev = moment();
         this.next = this.prev.clone().add(config.period, 'ms');
+    }
+
+    /**
+     * Returns JS representation of the COVTimer entity
+     *
+     * @return {Interfaces.COVTimer.Data}
+     */
+    public valueOf(): Interfaces.COVTimer.Data {
+        return { prev: this.prev, next: this.next };
     }
 }
