ff2e1cd3b7c7a2ad34eef2544fbeadcc219025a1
Geass <geass1000@gmail.com>
2018-05-18 19:38:51 +0300

    add: implementation of the `getSimulationLogic` method

--- src/devices/actors/hvac/room-control/room-control.device.ts
+++ src/devices/actors/hvac/room-control/room-control.device.ts
@@ -12,8 +12,19 @@ import * as Errors from '../../../../core/errors';
 
 import { store } from '../../../../redux';
 
+import { RoomControlSimulation } from './room-control.simulation';
+
 export class RoomControlActorDevice extends HVACActorDevice {
     public readonly className: string = 'RoomControlActorDevice';
     public state: Interfaces.Actor.RoomControl.State;
     public config: Interfaces.Actor.RoomControl.Config;
+
+    /**
+     * Creates the instance of the simulation logic.
+     *
+     * @return {RoomControlSimulation}
+     */
+    public getSimulationLogic (): RoomControlSimulation {
+        return new RoomControlSimulation(this.logger);
+    }
 }
