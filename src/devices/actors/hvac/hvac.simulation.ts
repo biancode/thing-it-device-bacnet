dcde4d94b62a70dae7cd63d3f79b969b24b8d788
Geass <geass1000@gmail.com>
2018-05-18 19:58:00 +0300

    add: hvac simulation logic (stub)

---
+++ src/devices/actors/hvac/hvac.simulation.ts
@@ -0,0 +1,12 @@
+import * as Simulations from '../../../core/simulations';
+
+export class HVACSimulation extends Simulations.BaseSimulation {
+
+    /**
+     * Starts the simulation logic for specific device.
+     *
+     * @return {void}
+     */
+    public startSimulation () {
+    }
+}
