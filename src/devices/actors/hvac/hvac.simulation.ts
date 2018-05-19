550d2418aeaf96930fdb51dc3cf5b77724374db9
Geass <geass1000@gmail.com>
2018-05-19 14:22:06 +0300

    add: `initParamsFromConfig` method (stub)

--- src/devices/actors/hvac/hvac.simulation.ts
+++ src/devices/actors/hvac/hvac.simulation.ts
@@ -1,6 +1,17 @@
 import * as Simulations from '../../../core/simulations';
 
 export class HVACSimulation extends Simulations.BaseSimulation {
+    /**
+     * Creates unit params of the BACnet Object from plugin configuration.
+     * Steps:
+     * - creates `setpointFeedbackObjectId`.
+     * - creates `temperatureObjectId`.
+     * - creates `setpointModificationObjectId`.
+     *
+     * @return {void}
+     */
+    public async initParamsFromConfig (): Promise<void> {
+    }
 
     /**
      * Starts the simulation logic for specific device.
