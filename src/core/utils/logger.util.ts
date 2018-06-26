5885805cd9db03594a1ace39dd6c54f0251c9077
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-06-26 16:21:30 +0300

    add: logger class

--- src/core/utils/logger.util.ts
+++ src/core/utils/logger.util.ts
@@ -1,6 +1,63 @@
 
-export abstract class Logger {
-    abstract logInfo(...messages: any[]): void;
-    abstract logDebug(...messages: any[]): void;
-    abstract logError(...messages: any[]): void;
+import * as Enums from '../enums';
+
+export class Logger {
+    private loggerMethods: Map<Enums.LogLevel, any> = new Map();
+
+    constructor (private tidDevice: any) {
+        this.tidDevice.loglevel = 'debug';
+    }
+
+    /**
+     * Sets the method name of the logger.
+     *
+     * @param  {Enums.LogLevel} methodName - name of the logger method
+     * @param  {any} method - logger method
+     * @return {void}
+     */
+    public setLogMethod (methodName: Enums.LogLevel, method: any): void {
+        this.loggerMethods.set(methodName, method);
+    }
+
+    /**
+     * Shows the log message with `Info` priority.
+     *
+     * @param  {...any} messages - log messages
+     * @return {void}
+     */
+    public logInfo (...messages: any[]): void {
+        this.log(Enums.LogLevel.Info, ...messages);
+    }
+
+    /**
+     * Shows the log message with `Debug` priority.
+     *
+     * @param  {...any} messages - log messages
+     * @return {void}
+     */
+    public logDebug (...messages: any[]): void {
+        this.log(Enums.LogLevel.Debug, ...messages);
+    }
+
+    /**
+     * Shows the log message with `Error` priority.
+     *
+     * @param  {...any} messages - log messages
+     * @return {void}
+     */
+    public logError (...messages: any[]): void {
+        this.log(Enums.LogLevel.Error, ...messages);
+    }
+
+    /**
+     * Shows the log message with specific priority.
+     *
+     * @param  {Enums.LogLevel} methodName - name of the logger method
+     * @param  {...any} messages - log messages
+     * @return {void}
+     */
+    private log (methodName: Enums.LogLevel, ...messages: any[]) {
+        const loggerMethod = this.loggerMethods.get(methodName);
+        loggerMethod.apply(this.tidDevice, messages);
+    }
 }
