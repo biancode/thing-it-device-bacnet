8652da4b98cb9fa5c47e592b9c4058c3b0d9e1d2
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-07-07 15:09:25 +0300

    add: example configuration and initialization tests

---
+++ tests/bacNetDevice.spec.js
@@ -0,0 +1,49 @@
+const Bluebird = require('bluebird');
+const assert = require('assert');
+const path = require('path');
+
+const TestDriver = require('thing-it-test');
+
+function wait (delay, fn) {
+    return new Bluebird((resolve, reject) => setTimeout(() => {
+        fn && fn();
+        resolve();
+    }, delay));
+}
+
+describe('[thing-it] BacNet Device - code test', () => {
+    let testDriver;
+
+    const time = {
+        test1: 15000,
+    }
+
+    before(() => {
+        testDriver = TestDriver.createTestDriver({ logLevel: 'debug' });
+        testDriver.registerDevicePlugin('bacnet', path.resolve(__dirname, '../bacNetDevice'));
+        testDriver.registerUnitPlugin(path.resolve(__dirname, '../default-units/binaryInput'));
+        testDriver.registerUnitPlugin(path.resolve(__dirname, '../default-units/binaryValue'));
+        testDriver.registerUnitPlugin(path.resolve(__dirname, '../default-units/binaryLight'));
+        testDriver.registerUnitPlugin(path.resolve(__dirname, '../default-units/analogInput'));
+        testDriver.registerUnitPlugin(path.resolve(__dirname, '../default-units/analogValue'));
+        testDriver.registerUnitPlugin(path.resolve(__dirname, '../default-units/jalousie'));
+        testDriver.registerUnitPlugin(path.resolve(__dirname, '../default-units/jalousieSimple'));
+        testDriver.registerUnitPlugin(path.resolve(__dirname, '../default-units/light'));
+        testDriver.registerUnitPlugin(path.resolve(__dirname, '../default-units/multiStateInput'));
+        testDriver.registerUnitPlugin(path.resolve(__dirname, '../default-units/multiStateValue'));
+        testDriver.registerUnitPlugin(path.resolve(__dirname, '../default-units/roomControl'));
+        testDriver.registerUnitPlugin(path.resolve(__dirname, '../default-units/thermostat'));
+        testDriver.start({
+            configuration: require('../examples/configuration.js'),
+            heartbeat: 10,
+        });
+    });
+
+    after(async () => {
+        await testDriver.stop();
+    });
+
+    it('should have completed initialization successfully', async () => {
+        await wait(time.test1);
+    }).timeout(time.test1 + 2000);
+});
