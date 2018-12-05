b3edd65e4e813c70a3faa09f008600f5520d83d3
Nikita Mikhaliuk <nikkita1992@mail.ru>
2018-12-05 13:41:48 +0300

    fix: remove double 'subscribeToProperty' call

--- default-units/analogInput.js
+++ default-units/analogInput.js
@@ -415,8 +415,6 @@ AnalogInput.prototype.subscribeToStatusCheck = function (interval) {
                     status: Enums.OperationalStatus.Pending,
                     message: 'Status check successful. Receiving properties...'
                 };
-                // Creates 'subscribtion' to the BACnet object properties
-                _this.subscribeToProperty();
 
                 // Inits the BACnet object properties
                 _this.initProperties();
--- default-units/analogValue.js
+++ default-units/analogValue.js
@@ -450,8 +450,6 @@ AnalogValue.prototype.subscribeToStatusCheck = function (interval) {
                     status: Enums.OperationalStatus.Pending,
                     message: 'Status check successful. Receiving properties...'
                 };
-                // Creates 'subscribtion' to the BACnet object properties
-                _this.subscribeToProperty();
 
                 // Inits the BACnet object properties
                 _this.initProperties();
