283c49da3544d65e010969922d0b217ca569030c
Nikita Mikhaliuk <nikkita1992@mail.ru>
2019-01-03 14:51:32 +0300

    add: mobiscroll library dependency for ui testbed

--- tests/uiTest/src/app/app.module.ts
+++ tests/uiTest/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MbscModule } from '@mobiscroll/angular';
 
 import { AppComponent } from './app.component';
 import { MatSliderModule } from '@angular/material';
@@ -28,7 +29,8 @@ import { MultiStateValueComponent } from './multi-state-value/multi-state-value.
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    MbscModule
   ],
   providers: [],
   bootstrap: [AppComponent]
